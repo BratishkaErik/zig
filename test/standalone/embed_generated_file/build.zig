ef882656342db39103dcb339d1f13cda6e987cc2
Eric Joldasov <bratishkaerik@landless-city.net>
2024-06-20 20:25:27 +0500

    test-standalone: migrate from deprecated std.Build API functions
    Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>

--- test/standalone/embed_generated_file/build.zig
+++ test/standalone/embed_generated_file/build.zig
@@ -4,22 +4,31 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Test it");
     b.default_step = test_step;
 
-    const bootloader = b.addExecutable(.{
-        .name = "bootloader",
-        .root_source_file = b.path("bootloader.zig"),
-        .target = b.resolveTargetQuery(.{
-            .cpu_arch = .x86,
-            .os_tag = .freestanding,
-        }),
-        .optimize = .ReleaseSmall,
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .x86,
+        .os_tag = .freestanding,
     });
 
-    const exe = b.addTest(.{
+    const bootloader = b.addExecutable2(.{
+        .name = "bootloader",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("bootloader.zig"),
+            .target = target,
+            .optimize = .ReleaseSmall,
+        }),
+    });
+
+    const main_mod = b.createModule(.{
         .root_source_file = b.path("main.zig"),
+        .target = target,
         .optimize = .Debug,
     });
-    exe.root_module.addAnonymousImport("bootloader.elf", .{
+    main_mod.addAnonymousImport("bootloader.elf", .{
         .root_source_file = bootloader.getEmittedBin(),
+    });
+
+    const exe = b.addTest2(.{
+        .root_module = main_mod,
     });
 
     // TODO: actually check the output
